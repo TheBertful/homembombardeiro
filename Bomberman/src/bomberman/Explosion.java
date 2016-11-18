e0d49654dcd4524c135e9ad6c1bfaf34749513e1
TheBertful <Administrador@LAB6M13.unifil.br>
2016-11-18 19:49:37 -0200

    classe Explosion

---
+++ Bomberman/src/bomberman/Explosion.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bomberman;
+
+import java.awt.Image;
+import java.awt.Rectangle;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Explosion {
+    
+    private Image explosion;
+    private String explosionPath = "/Assets/firecenter2.png";
+    private int x;
+    private int y;
+    private int lifetime = 500;
+    private Rectangle hitbox;
+    
+    public Explosion(int x, int y){
+        this.x = x;
+        this.y = y;
+        inicializarExplosao();
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getLifetime() {
+        return lifetime;
+    }
+
+    public void setLifetime(int lifetime) {
+        this.lifetime = lifetime;
+    }
+
+    public Rectangle getHitbox() {
+        return hitbox;
+    }
+    
+    private void inicializarExplosao() {
+        ImageIcon ii = new ImageIcon(this.getClass().getResource(explosionPath));
+        explosion = ii.getImage();
+        hitbox = new Rectangle(x, y, ii.getIconWidth(), ii.getIconHeight());
+    }
+    
+    
+    
+}
